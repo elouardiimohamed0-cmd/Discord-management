d6a678f21fb52ad0796c551299663f5f83e047b5
A999ESCANOR <elouardiimohamed0@gmail.com>
2026-06-11 14:17:08 +0200

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 # ✅ FREE model
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
 
 # ✅ Base function
